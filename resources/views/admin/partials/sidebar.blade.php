6f6424dd3d53377b6d8a11f9823879dac2d3eb71
SBTHDR <subrata.halder13d@gmail.com>
2021-10-21 20:03:43 +0600

    Sidebar Cleanup

--- resources/views/admin/partials/sidebar.blade.php
+++ resources/views/admin/partials/sidebar.blade.php
@@ -19,28 +19,24 @@
             <!-- sidebar menu -->
             <ul class="nav sidebar-inner" id="sidebar-menu">
 
-                <li class="has-sub active expand">
+                <li class="has-sub">
                     <a class="sidenav-item-link" href="javascript:void(0)" data-toggle="collapse"
                         data-target="#dashboard" aria-expanded="false" aria-controls="dashboard">
                         <i class="mdi mdi-view-dashboard-outline"></i>
                         <span class="nav-text">Dashboard</span> <b class="caret"></b>
                     </a>
-                    <ul class="collapse show" id="dashboard" data-parent="#sidebar-menu">
+                    <ul class="collapse" id="dashboard" data-parent="#sidebar-menu">
                         <div class="sub-menu">
 
-                            <li class="active">
+                            <li class="">
                                 <a class="sidenav-item-link" href="index.html">
                                     <span class="nav-text">Ecommerce</span>
-
                                 </a>
                             </li>
 
                             <li>
                                 <a class="sidenav-item-link" href="analytics.html">
                                     <span class="nav-text">Analytics</span>
-
-                                    <span class="badge badge-success">new</span>
-
                                 </a>
                             </li>
 
@@ -98,26 +94,6 @@
 
                                         <li>
                                             <a href="sign-up.html">Sign Up</a>
-                                        </li>
-
-                                    </div>
-                                </ul>
-                            </li>
-
-                            <li class="has-sub">
-                                <a class="sidenav-item-link" href="javascript:void(0)" data-toggle="collapse"
-                                    data-target="#others" aria-expanded="false" aria-controls="others">
-                                    <span class="nav-text">Others</span> <b class="caret"></b>
-                                </a>
-                                <ul class="collapse" id="others">
-                                    <div class="sub-menu">
-
-                                        <li>
-                                            <a href="invoice.html">invoice</a>
-                                        </li>
-
-                                        <li>
-                                            <a href="error.html">Error</a>
                                         </li>
 
                                     </div>
