aece5e1639ecefb078d076c49842d11c0926b696
SBTHDR <subrata.halder13d@gmail.com>
2021-10-25 21:01:48 +0600

    Added Product Discount

--- app/Http/Controllers/Frontend/HomeController.php
+++ app/Http/Controllers/Frontend/HomeController.php
@@ -15,7 +15,8 @@ class HomeController extends Controller
         $categories = Category::orderBy('category_name_en', 'ASC')->get();
         $sliders = Slider::orderBy('id', 'DESC')->limit(3)->get();
         $products = Product::orderBy('id', 'DESC')->get();
-        return view('frontend.index', compact('categories', 'sliders', 'products'));
+        $hotDeals = Product::where('hot_deals', 1)->orderBy('id', 'DESC')->limit(3)->get();
+        return view('frontend.index', compact('categories', 'sliders', 'products', 'hotDeals'));
     }
 
     public function show($id)
