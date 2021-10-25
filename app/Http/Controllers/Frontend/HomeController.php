9cf40fa257d9db9618c0f296bc726c1842b45225
SBTHDR <subrata.halder13d@gmail.com>
2021-10-25 14:48:23 +0600

    Added Frontend Category

--- app/Http/Controllers/Frontend/HomeController.php
+++ app/Http/Controllers/Frontend/HomeController.php
@@ -3,12 +3,14 @@
 namespace App\Http\Controllers\Frontend;
 
 use App\Http\Controllers\Controller;
+use App\Models\Category;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
     public function index()
     {
-        return view('frontend.index');
+        $categories = Category::orderBy('category_name_en', 'ASC')->get();
+        return view('frontend.index', compact('categories'));
     }
 }
