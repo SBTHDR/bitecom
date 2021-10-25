ddd02d40a417389c85abf4ff37b73f3b8f6eafb2
SBTHDR <subrata.halder13d@gmail.com>
2021-10-25 17:19:31 +0600

    Added Product by Category

--- app/Http/Controllers/Frontend/HomeController.php
+++ app/Http/Controllers/Frontend/HomeController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers\Frontend;
 
 use App\Http\Controllers\Controller;
 use App\Models\Category;
+use App\Models\Product;
+use App\Models\Slider;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
@@ -11,6 +13,8 @@ class HomeController extends Controller
     public function index()
     {
         $categories = Category::orderBy('category_name_en', 'ASC')->get();
-        return view('frontend.index', compact('categories'));
+        $sliders = Slider::orderBy('id', 'DESC')->limit(3)->get();
+        $products = Product::orderBy('id', 'DESC')->get();
+        return view('frontend.index', compact('categories', 'sliders', 'products'));
     }
 }
