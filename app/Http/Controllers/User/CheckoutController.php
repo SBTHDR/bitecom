f3d9a3d1e8ad62aa4c902f0d432744b7f0c9c700
SBTHDR <subrata.halder13d@gmail.com>
2022-01-17 14:20:28 +0600

    Stripe Payment Config

--- app/Http/Controllers/User/CheckoutController.php
+++ app/Http/Controllers/User/CheckoutController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\User;
 
 use App\Http\Controllers\Controller;
+use Gloudemans\Shoppingcart\Facades\Cart;
 use Illuminate\Http\Request;
 
 class CheckoutController extends Controller
@@ -14,10 +15,12 @@ class CheckoutController extends Controller
     	$data['shipping_phone'] = $request->shipping_phone;
     	$data['post_code'] = $request->post_code;
     	$data['address'] = $request->address;
+        $cartTotal = Cart::total();
 
 
     	if ($request->payment_method == 'stripe') {
-    		return view('frontend.payment.stripe',compact('data'));
+    		// return view('frontend.payment.stripe',compact('data'));
+            return view('frontend.payment.stripe',compact('data','cartTotal'));
     	}elseif ($request->payment_method == 'card') {
     		return 'card';
     	}else{
